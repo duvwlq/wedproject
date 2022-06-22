b513c471fc3f262451189d7fa35e908218bc216d
duvwlq <101647121+duvwlq@users.noreply.github.com>
2022-06-22 14:20:31 +0000

    END

--- start/js/start.js
+++ start/js/start.js
@@ -1,6 +1,7 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
+
 const endPoint = 12;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -8,6 +9,23 @@ function calResult(){
   console.log(select);
   var result = select.indexOf(Math.max(...select));
   return result;
+}
+
+function setResult(){
+  let point = calResult();
+  const resultName = document.querySelector('.resultname');
+  resultName.innerHTML = infoList[point].name;
+
+  var resultImg = document.createElement('img');
+  const imgDiv = document.querySelector('#resultImg');
+  var imgURL = 'img/image-' + point + '.png';
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add('img-fluid');
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector('.resultDesc');
+  resultDesc.innerHTML = infoList[point].desc;
 }
 
 function goResult(){
@@ -20,10 +38,10 @@ function goResult(){
       qna.style.display = "none";
       result.style.display = "block"
     }, 450)})
-    console.log(select)
+    setResult();
 }
 
-function addAnswer(answerText, qIdx,idx){
+function addAnswer(answerText, qIdx, idx){
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
   answer.classList.add('answerList');
@@ -43,7 +61,11 @@ function addAnswer(answerText, qIdx,idx){
       children[i].style.animation = "fadeOut 0.5s";
     }
     setTimeout(() => {
-      select[qIdx] = idx;
+      var target = qnaList[qIdx].a[idx].type;
+      for(let i = 0; i < target.length; i++){
+        select[target[i]] += 1;
+      }
+
       for(let i = 0; i < children.length; i++){
         children[i].style.display = 'none';
       }
@@ -53,14 +75,15 @@ function addAnswer(answerText, qIdx,idx){
 }
 
 function goNext(qIdx){
-    if(qIdx == endPoint){
-        goResult();
-        return;
-    }
+  if(qIdx === endPoint){
+    goResult();
+    return;
+  }
+
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for(let i in qnaList[qIdx].a){
-    addAnswer(qnaList[qIdx].a[i].answer, qIdx,i);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
   var status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qIdx+1) + '%';
